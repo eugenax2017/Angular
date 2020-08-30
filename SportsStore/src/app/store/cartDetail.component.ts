e059f66ff145fa0000c460226a26981283474e8a
Eugene <eugenax2017@gmail.com>
2020-08-30 20:10:58 +0200

    Sport store

---
+++ SportsStore/src/app/store/cartDetail.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
+import { ConnectionService } from "../model/connection.service";
+
+@Component({
+    //template: `<div><h3 class="bg-info p-1 text-white">Cart Detail Component</h3></div>`
+    templateUrl: "cartDetail.component.html"
+})
+
+export class CartDetailComponent {
+    public connected: boolean = true;
+
+    constructor(public cart: Cart, private connection: ConnectionService) {
+        this.connected = this.connection.connected;
+        connection.Changes.subscribe((state) => this.connected = state);
+
+    }
+}
